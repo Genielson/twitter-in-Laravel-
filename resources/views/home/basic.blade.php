714f76e628bc34e8aebdbaed2e5b10ca9c225f4d
Genielson <ge.nielson@hotmail.com>
2022-11-05 16:13:14 -0300

    Adicionando arquivo basic.blade.php para organizar a view home

---
+++ resources/views/home/basic.blade.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home | Twitter</title>
+
+    <link rel="shortcut icon" type="image/png" href="assets/images/twitter.svg">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/home_style.css">
+
+
+</head>
+<body>
+
+
+
+
+    <script src="assets/js/search.js"></script>
+          <script src="assets/js/photo.js?v=<?php echo time(); ?>"></script>
+          <script type="text/javascript" src="assets/js/hashtag.js"></script>
+          <script type="text/javascript" src="assets/js/like.js"></script>
+          <script type="text/javascript" src="assets/js/comment.js?v=<?php echo time(); ?>"></script>
+          <script type="text/javascript" src="assets/js/retweet.js?v=<?php echo time(); ?>"></script>
+          <script type="text/javascript" src="assets/js/follow.js?v=<?php echo time(); ?>"></script>
+      <script src="https://kit.fontawesome.com/38e12cc51b.js" crossorigin="anonymous"></script>
+      <!-- <script src="assets/js/jquery-3.4.1.slim.min.js"></script> -->
+      <script src="assets/js/jquery-3.5.1.min.js"></script>
+
+        <script src="assets/js/popper.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
