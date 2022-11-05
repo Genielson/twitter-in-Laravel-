860a79056e5dc34dbc582eb5bfe0ec042005ba5e
Genielson <ge.nielson@hotmail.com>
2022-11-05 15:04:58 -0300

    Adicionando footer com imports Js de login em footer.blade.php

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,38 @@
+<footer>
+                <ul>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Help Center</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Status</a></li>
+                    <li><a href="#">Jobs</a></li>
+                    <li><a href="#">Terms</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Cookies</a></li>
+                    <li><a href="#">Ads info</a></li>
+                    <li><a href="#">Brand</a></li>
+                    <li><a href="#">Apps</a></li>
+                    <li><a href="#">Advertise</a></li>
+                    <li><a href="#">Marketing</a></li>
+                    <li><a href="#">Businesses</a></li>
+                    <li><a href="#">Developers</a></li>
+                    <li><a href="#">Directory</a></li>
+                    <li><a href="#">Settings</a></li>
+                    <li>© 2021 Twitter Clone</li>
+                </ul>
+            </footer>
+        </main>
+        
+        <script src="assets/js/jquery-3.4.1.slim.min.js"></script>
+        <script src="assets/js/popper.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/mine.js"></script>
+
+
+        <script>  
+             $(document).ready(function(){
+                $("#exampleModalCenter").modal('show');
+            });
+        </script>
+        
+</body>
+</html>
