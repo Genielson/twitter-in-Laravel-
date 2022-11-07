a01acd19ceba85a843ccc8ce8238d22c983f4eab
Genielson <ge.nielson@hotmail.com>
2022-11-06 22:24:02 -0300

    Adicionando funcionalidade de criacao de usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Controllers\UserAuthenticationController;
 class UserController extends Controller
 {
 
     public function createUser($name,$email,$password){
-
-
+        $user = new User();
+        $user->email = $email;
+        $user->username = $name;
+        $user->password = $password;
+        $user->save();
+        $user->login($email,$password);
+        return redirect()->route('home');
     }
 
 }
